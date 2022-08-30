ee1264777a4cb02994db66be37eac29673a6e47d
rails s <mesanovicmahir@gmail.com>
2022-08-30 22:17:13 +0200

    init

--- index.php
+++ index.php
@@ -68,6 +68,57 @@
 </form>
 </div>
 </section>
+<section class="prvi">
+<section class="sekcija3">
+
+<div class="tekst">
+    <img src="E.jpg"/>
+    <h4>Dobrodosli u Hotel KIM</h4>
+    <h1>Vas drugi dom</h1>
+    <p>___________________</p>
+    <p>U centru grada a opet izdvojen od gradske vreve Hotel Kim 
+        je mjesto gdje ćete biti dočekani kao član porodice bez obzira 
+        da li ste kod nas prvi put ili ste naš stalni gost. Već od 1996. 
+        se bavimo ugostiteljstvom a 2006. smo proširili naše usluge i na 
+        hotelijerstvo. Od tada rastemo i  radimo na kvaliteti naših usluga.
+         Raspolažemo sa 32 sobe i 5 apartmana. Pored smještajnih jedinica u 
+         bašti našeg hotela ili na hotelskoj terasi možete predahnuti i uživati 
+         uz naše specijalitete domaće i internacionalne kuhinje.  </p>
+         <button>Detaljno</button>
+</div>
+
+</section>
+
+<section class="sekcija4">
+    <h1>Specijalna ponuda</h1>
+    <div class="kutija">
+        <h3>1.Specijalna ponuda smjestaja</h3><br>
+        <p>Platite tri noćenja po punoj cijeni četvrta noć gratis. 
+            Ponuda uključuje: Noćenje sa doručkom, besplatan WiFi u svim dijelovima 
+            objekta, besplatan parking prostor u sklopu hotela, besplatan prijevoz do 
+            aerodroma i još niz pogodnosti. Kontaktirajte nas za rezervacije i iskoristite 
+            ovu priliku da posjetite Sarajevo i Ilidžu i uživate u bogatoj turističkoj ponudi.</p>
+</div>
+<div class="kutija">
+        <h3>1.Specijalna ponuda smjestaja</h3><br>
+        <p>Platite tri noćenja po punoj cijeni četvrta noć gratis. 
+            Ponuda uključuje: Noćenje sa doručkom, besplatan WiFi u svim dijelovima 
+            objekta, besplatan parking prostor u sklopu hotela, besplatan prijevoz do 
+            aerodroma i još niz pogodnosti. Kontaktirajte nas za rezervacije i iskoristite 
+            ovu priliku da posjetite Sarajevo i Ilidžu i uživate u bogatoj turističkoj ponudi.</p>
+</div>
+<div class="kutija">
+        <h3>1.Specijalna ponuda smjestaja</h3><br>
+        <p>Platite tri noćenja po punoj cijeni četvrta noć gratis. 
+            Ponuda uključuje: Noćenje sa doručkom, besplatan WiFi u svim dijelovima 
+            objekta, besplatan parking prostor u sklopu hotela, besplatan prijevoz do 
+            aerodroma i još niz pogodnosti. Kontaktirajte nas za rezervacije i iskoristite 
+            ovu priliku da posjetite Sarajevo i Ilidžu i uživate u bogatoj turističkoj ponudi.</p>
+</div>
+</section>
+
+
+</section>
 </body>
 
 
