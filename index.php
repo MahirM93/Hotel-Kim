133651be3e816a3fadb8524219651f27b6b68bd7
rails s <mesanovicmahir@gmail.com>
2022-08-30 20:33:40 +0200

    init

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<head>
+<title>Hotel</title>
+<link rel="stylesheet"href="style.css">
+</head>
+<body>
+<header>
+
+<div class="banner">
+    <div class="navbar">
+        <img src="logo.png"class="logo">
+        <ul>
+            <li><a href="#">Pocetna</a></li>
+            <li><a href="#">Smjestaj</a></li>
+            <li><a href="#">Restorani</a></li>
+            <li><a href="#">O nama</a></li>
+            <li><a href="#">Kontakt</a></li>
+            
+        </ul>
+</div>
+
+<div class="content">
+    <h1>Zdravo svima</h1>  <p>Dobro dosli na  web stranicu hotela</p>
+    <div>
+        <button type="button"><span></span>Lokacija</button>
+        <button type="button"><span></span>Reservacija</button>
+    </div>
+
+</div>
+
+
+
+</header>
+</body>
+
+
+
+</html>
