22148df1caced8c78288e180f5696e8e6f7a8577
rails s <mesanovicmahir@gmail.com>
2022-08-30 21:19:36 +0200

    init

--- index.php
+++ index.php
@@ -28,9 +28,46 @@
 
 </div>
 
-
-
 </header>
+<section class="section2">
+
+<div class="rezervacija">
+    <h2>Rezervacija Soba</h2>
+    <form metho="post"target="self">
+   <div class="moj">
+   <h4>Vrsta</h4>     
+<select name="vrsta">
+    <option value="jednosobna">Jednokrevetna Soba</option>
+    <option value="jednosobna">Dvokrevetna Soba</option>
+    <option value="jednosobna">Trokrevetna Soba</option>
+    <option value="jednosobna">Apartman</option>
+</select>
+</div>
+<div class="moj">
+   <h4>Dolazak</h4>     
+<input type="date"name="datum"value="<?php echo date('Y-m-d');?>"/>
+</div>
+<div class="moj">
+   <h4>Odlazak</h4>     
+   <input type="date"name="datum"value="<?php echo date('Y-m-d');?>"/>
+</div>
+<div class="moj">
+   <h4>Odrasli</h4>     
+   <select name="vrsta">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+</select>
+</div>
+<div class="moj">
+   <h4>Djeca</h4>     
+<input type="text"name=""placeholder=""require/>
+</div>
+<input type="submit"value="Rezervisi"/>
+</form>
+</div>
+</section>
 </body>
 
 
